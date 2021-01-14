2d6e370daefbc04fff44fb44dbbb29ce341634c8
Oleg Baskakov <oleg.baskakov@appfolio.com>
2021-01-14 15:54:17 -0800

    add a default parameter to a card

--- game/CardDTO.ts
+++ game/CardDTO.ts
@@ -1,10 +1,20 @@
-export default class CardDTO {
+class ACardDTO {
+
     id?: number
     unitType: string
 
-    constructor(props: CardDTO) {
+    constructor(props: ACardDTO) {
         this.id = props.id
         this.unitType = props.unitType
+    }
+}
+
+export default class CardDTO extends ACardDTO {
+    isSelected: boolean
+
+    constructor(props: ACardDTO & {isSelected?: boolean}) {
+        super(props)
+        this.isSelected = props.isSelected ?? false
     }
 
 }
